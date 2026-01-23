31721c5d7eecfc50a996fef076e1e0a0fd75efac
Kateryna Mokliakova <kateryna_mokliakova@epam.com>
2026-01-23 15:37:24 +0100

    staging interview incremental improved

--- dbt_interview_project/models/staging/stg_interview.sql
+++ dbt_interview_project/models/staging/stg_interview.sql
@@ -1,4 +1,12 @@
-{{ config(materialized='table') }}
+{{ 
+  config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='id',
+    on_schema_change='append_new_columns'
+  ) 
+}}
+
 {{ stg_model(
     source_name='interviews', 
     table_name='interviews', 
@@ -6,3 +14,10 @@
     updated_column='updated_at', 
     sort_col='offset'
 ) }}
+
+{% if is_incremental() %}  
+where updated_at > (  
+  select max(updated_at)  
+  from {{ this }}  
+)  
+{% endif %}
